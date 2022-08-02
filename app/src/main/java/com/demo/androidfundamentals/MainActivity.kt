af2b308825f88716b1bd9e5cc4309f411ac2c671
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-08-02 11:29:40 +0530

    integrated ViewModel

--- app/src/main/java/com/demo/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/demo/androidfundamentals/MainActivity.kt
@@ -2,17 +2,28 @@ package com.demo.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.demo.androidfundamentals.databinding.ActivityMainBinding
 import com.demo.androidfundamentals.models.MovieModel
+import com.demo.androidfundamentals.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    //ViewBinding
     private lateinit var binding: ActivityMainBinding
+
+    //ViewModel
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.demoText.text = viewModel.textWorking
     }
 }
---
+++ app/src/main/java/com/demo/androidfundamentals/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.demo.androidfundamentals.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val textWorking = "ViewModel Working";
+}
