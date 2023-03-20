d199ba592aa39a980dbeeff6fb0fe49b902370d5
shadabhashmi <er.shadabhashmi@gmail.com>
2023-03-20 22:13:18 +0530

    feat: resource file

---
+++ app/src/main/java/com/demo/androidfundamentals/core/Resource.kt
@@ -0,0 +1,33 @@
+package com.demo.androidfundamentals.core
+
+import com.demo.androidfundamentals.models.MovieModel
+
+data class Resource<ResultType>(
+    var status: Status,
+    var data: ResultType? = null,
+    var errorMessage: String? = null
+){
+    companion object {
+        /**
+         * Creates [Resource] object with `SUCCESS` status and [data].
+         * Returning object of Resource(Status.SUCCESS, data, null)  last value is null so passing it optionally
+         */
+        fun <ResultType> success(data: ResultType): Resource<ResultType> =
+            Resource(Status.Success(data as List<MovieModel>))
+
+        /**
+         * Creates [Resource] object with `LOADING` status to notify
+         * the UI to showing loading.
+         * Returning object of Resource(Status.SUCCESS, null, null) last two values are null so passing them optionally
+         */
+        fun <ResultType> loading(): Resource<ResultType> = Resource(Status.Loader)
+
+        /**
+         * Creates [Resource] object with `ERROR` status and [message].
+         * Returning object of Resource(Status.ERROR, errorMessage = message)
+         */
+        fun <ResultType> error(message: String?): Resource<ResultType> =
+            Resource(Status.Error(message!!))
+
+    }
+}
--- app/src/main/java/com/demo/androidfundamentals/retrofit/RetrofitAPI.kt
+++ app/src/main/java/com/demo/androidfundamentals/retrofit/RetrofitAPI.kt
@@ -1,6 +1,7 @@
 package com.demo.androidfundamentals.retrofit
 
 import com.demo.androidfundamentals.api.MoviesAPI
+import com.demo.androidfundamentals.core.Resource
 import com.demo.androidfundamentals.models.APIModel
 import retrofit2.Response
 import retrofit2.http.GET
